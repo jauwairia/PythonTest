bf8c86fe05c4f036812e002c05c53da156895270
jauwairia <jauwairia@rit.kaist.ac.kr>
2018-05-09 15:26:37 +0200

    Third_commit

--- Tutorial.py
+++ Tutorial.py
@@ -3,3 +3,44 @@
 print('All things python')
 print('Change')
 
+#Single comment
+
+'''Multiple Line 
+Comment'''
+
+'''
+Five Data Types
+
+1. Numbers
+2. Strings
+3. Lists
+4. Tuples
+5. Dictionaries
+
+Seven Arithmetic Operations
+
+1. +
+2. -
+3. *
+4. /
+5. % Modulus
+6. ** Exponential Multiplication 
+7. // Divides and removes remainder 
+
+'''
+
+name= "Invisible"
+print(name)
+
+print("5*2=", 5*2)
+
+single_line_string = "You can do whatever you aim for"
+multiple_line_string = ''' I dont like when people are selfish and do not care
+for others feelings'''
+
+# Play around with different ways of printing strings
+print('I want to say that', single_line_string, multiple_line_string)
+
+# concatenating the strings together
+print(single_line_string + multiple_line_string)
+print(" %s %s %s " % ("I want to say that", single_line_string, multiple_line_string))
